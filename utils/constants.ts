e5465a865116add971645b5742446af29f83397e
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-04-12 01:05:43 +0200

    feat(utils): add constants
    fix: x

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,1 +1,16 @@
-export const CYPRESS_BASE_URL = 'http://localhost:3000'
+import { LocaleObject } from "@nuxtjs/i18n/dist/runtime/composables";
+
+export const CYPRESS_BASE_URL = "http://localhost:3000";
+export const LOCALES: LocaleObject[] = [
+  {
+    code: "en",
+    name: "English",
+    iso: "en", // Will be used as catchall locale by default.
+  },
+  {
+    code: "de",
+    name: "Deutsch",
+    iso: "de",
+  },
+];
+export const SITE_NAME = "Vio";
