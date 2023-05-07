853a00e4051d2da13cf2412dffb9cec403e733cd
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-05-07 17:05:33 +0200

    feat(legal)!: extract components
    BREAKING CHANGE: changes to app configuration required

--- .playground/app.config.ts
+++ .playground/app.config.ts
@@ -1,3 +1,24 @@
 export default defineAppConfig({
+  privacyPolicy: {
+    hostingCdn: {
+      external: {
+        address: {
+          city: '12345 City, Country',
+          name: 'Company Name',
+          street: 'Street 0',
+        },
+      },
+    },
+    mandatoryInfo: {
+      responsible: {
+        address: {
+          city: '12345 City, Country',
+          email: "E-Mail: contact{'@'}doma.in",
+          name: 'Firstname Lastname',
+          street: 'Street 0',
+        },
+      },
+    },
+  },
   siteName: 'Playground',
 })
