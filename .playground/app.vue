1dca7a4cd650826d3b31d33e114228cebe1b2110
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-07-28 15:14:27 +0200

    chore(deps): upgrade @nuxtjs/i18n-edge to v8.0.0-beta.13

--- .playground/app.vue
+++ .playground/app.vue
@@ -1,6 +1,19 @@
 <template>
   <VioApp
-    site-description="Vio is @dargmuesli's Nuxt layer."
-    og-image-alt="Social preview image for a Vio webpage."
+    :site-description="t('siteDescription')"
+    :og-image-alt="t('ogImageAlt')"
   />
 </template>
+
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
+<i18n lang="yml">
+de:
+  siteDescription: Vio ist {'@'}dargmueslis Nuxt layer.
+  ogImageAlt: Social Preview Image für eine Vio Webseite.
+en:
+  siteDescription: Vio is {'@'}dargmuesli's Nuxt layer.
+  ogImageAlt: Social Preview Image for a Vio webpage.
+</i18n>
---
+++ i18n.config.ts
@@ -0,0 +1,6 @@
+export const i18nConfig = {
+  fallbackWarn: false, // covered by linting
+  missingWarn: false, // covered by linting
+}
+
+export default defineI18nConfig(() => i18nConfig)
