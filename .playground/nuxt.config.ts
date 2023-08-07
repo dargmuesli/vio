22e5bcaf889c91316f3c04a974d6994cc3848d9a
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-08-07 20:54:44 +0200

    feat(playground): make name more specific

--- .playground/nuxt.config.ts
+++ .playground/nuxt.config.ts
@@ -4,6 +4,6 @@ export default defineNuxtConfig({
   extends: '@dargmuesli/nuxt-vio',
   i18n: I18N_MODULE_CONFIG, // `langDir`, `lazy` and `locales` must be configured to extend a layer having lazy-loaded translations (https://v8.i18n.nuxtjs.org/guide/layers#locales)
   site: {
-    name: 'Playground',
+    name: 'Vio Playground',
   },
 })
