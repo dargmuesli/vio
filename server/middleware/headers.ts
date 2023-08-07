da4c4576c6f5cb5e096509415360ef0e321e6099
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-08-07 04:03:19 +0200

    feat(headers): use app configuration's csp

--- server/middleware/headers.ts
+++ server/middleware/headers.ts
@@ -1,13 +1,44 @@
-import { defineEventHandler } from 'h3'
+import { appendHeader, defineEventHandler } from 'h3'
+import type { H3Event } from 'h3'
+import { AppConfig } from 'nuxt/schema'
 
-export default defineEventHandler((event) => {
-  const { res } = event.node
+import { TIMEZONE_HEADER_KEY } from '../../utils/constants'
+import { getTimezone } from '../../utils/networking'
 
-  res.setHeader('Permissions-Policy', '')
-
-  // // Disabled until there is better browser support (https://caniuse.com/?search=report-to)
-  // res.setHeader(
-  //   'Report-To',
-  //   '{"group":"default","max_age":31536000,"endpoints":[{"url":"https://dargmuesli.report-uri.com/a/d/g"}],"include_subdomains":true}'
-  // )
+export default defineEventHandler(async (event) => {
+  setRequestHeader(event, TIMEZONE_HEADER_KEY, await getTimezone(event))
+  // setContentSecurityPolicy(event);
+  setResponseHeaders(event)
 })
+
+// const setContentSecurityPolicy = (event: H3Event) => {
+//   const config = useAppConfig();
+
+//   appendHeader(
+//     event,
+//     "Content-Security-Policy",
+//     getCspAsString(config.public.vio.server.middleware.headers.csp)
+//   );
+// };
+
+const setRequestHeader = (event: H3Event, name: string, value: string) => {
+  event.node.req.headers[name] = value
+}
+
+const setResponseHeaders = (event: H3Event) => {
+  const config = useAppConfig() as AppConfig
+
+  for (const entry of Object.entries(
+    config.vio.server.middleware.headers.csp.default,
+  )) {
+    appendHeader(event, entry[0], entry[1])
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    for (const entry of Object.entries(
+      config.vio.server.middleware.headers.csp.production,
+    )) {
+      appendHeader(event, entry[0], entry[1])
+    }
+  }
+}
