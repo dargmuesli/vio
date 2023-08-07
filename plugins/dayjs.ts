e8360fadf80d71a9dd97011dbe3bcdd67e3b6569
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-08-07 03:57:08 +0200

    feat(plugins): add content

---
+++ plugins/dayjs.ts
@@ -0,0 +1,46 @@
+import dayjs, { extend, locale } from 'dayjs'
+
+// workaround for [1]
+import de from 'dayjs/locale/de'
+// import 'dayjs/locale/de' does not make locale available
+
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
+import timezone from 'dayjs/plugin/timezone'
+import utc from 'dayjs/plugin/utc'
+
+export default defineNuxtPlugin((_nuxtApp) => {
+  extend(isSameOrBefore)
+  extend(localizedFormat)
+  extend(timezone)
+  extend(utc)
+
+  // workaround for [1]
+  locale(de)
+  // dayjs.locale(en) makes `format` error
+
+  return {
+    provide: {
+      dayjs,
+    },
+  }
+})
+
+// declare module '#app' {
+//   interface NuxtApp {
+//     $dayjs: DayjsFn
+//   }
+// }
+
+// declare module 'nuxt/dist/app/nuxt' {
+//   interface NuxtApp {
+//     $dayjs: DayjsFn
+//   }
+// }
+
+/*
+  [1]
+  https://github.com/nuxt/framework/issues/7534#issuecomment-1248596609
+  https://github.com/nuxt/framework/issues/7206
+  https://github.com/maevsi/maevsi/issues/956
+*/
