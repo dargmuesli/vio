deaf9173fb01bed5a32dc00781437f832de36a02
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-08-07 20:55:27 +0200

    refactor(networking): extract source function from composable

--- composables/useGetServiceHref.ts
+++ composables/useGetServiceHref.ts
@@ -19,29 +19,3 @@ export const useGetServiceHref = () => {
       stagingHost: config.public.stagingHost,
     })
 }
-
-export const getServiceHref = ({
-  host,
-  isSsr = true,
-  name,
-  port,
-  stagingHost,
-}: {
-  host: string
-  isSsr?: boolean
-  name?: string
-  port?: number
-  stagingHost?: string
-}) => {
-  const nameSubdomain = name?.replaceAll('_', '-')
-  const nameSubdomainString = nameSubdomain ? `${nameSubdomain}.` : ''
-  const portString = port ? `:${port}` : ''
-
-  if (stagingHost) {
-    return `https://${nameSubdomainString}${stagingHost}`
-  } else if (isSsr && process.server) {
-    return `http://${name}${portString}`
-  } else {
-    return `https://${nameSubdomainString}${getDomainTldPort(host)}`
-  }
-}
--- utils/networking.ts
+++ utils/networking.ts
@@ -80,6 +80,32 @@ export const getHost = (req: IncomingMessage) => {
   return req.headers.host
 }
 
+export const getServiceHref = ({
+  host,
+  isSsr = true,
+  name,
+  port,
+  stagingHost,
+}: {
+  host: string
+  isSsr?: boolean
+  name?: string
+  port?: number
+  stagingHost?: string
+}) => {
+  const nameSubdomain = name?.replaceAll('_', '-')
+  const nameSubdomainString = nameSubdomain ? `${nameSubdomain}.` : ''
+  const portString = port ? `:${port}` : ''
+
+  if (stagingHost) {
+    return `https://${nameSubdomainString}${stagingHost}`
+  } else if (isSsr && process.server) {
+    return `http://${name}${portString}`
+  } else {
+    return `https://${nameSubdomainString}${getDomainTldPort(host)}`
+  }
+}
+
 export const getTimezone = async (event: H3Event) =>
   getCookie(event, TIMEZONE_COOKIE_NAME) ||
   (
