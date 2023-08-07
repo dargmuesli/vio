c4f6d65713abf5603ee5d229b1bf37222ec4fa6b
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-08-07 04:04:52 +0200

    feat(nuxt): add staging host to runtime configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -56,6 +56,11 @@ export default defineNuxtConfig({
       },
       isInProduction: process.env.NODE_ENV === 'production',
       isTesting: false,
+      stagingHost:
+        process.env.NODE_ENV !== 'production' &&
+        !process.env.NUXT_PUBLIC_STACK_DOMAIN
+          ? 'jonas-thelemann.de'
+          : undefined,
     },
   },
   typescript: {
