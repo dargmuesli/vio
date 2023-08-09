b07dc91ec4518323b70d97642159a3aace89b932
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-08-09 02:15:30 +0200

    fix(types): import type reference

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -1,7 +1,9 @@
-export type BackendError = CombinedError | { errcode: string; message: string }
+import type { CombinedError } from '@urql/core'
 
 export type ApiData = ComputedRef<{
   data?: Object
   errors: BackendError[]
   isFetching: boolean
 }>
+
+export type BackendError = CombinedError | { errcode: string; message: string }
