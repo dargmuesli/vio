47d5b177ed41da1d3a91635578bf18e5b386194e
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-08-31 00:43:36 +0200

    fix(app-layout): use server head composable

--- src/composables/useAppLayout.ts
+++ src/composables/useAppLayout.ts
@@ -2,13 +2,13 @@ export const useAppLayout = () => {
   const appConfig = useAppConfig()
   const siteConfig = useSiteConfig()
 
-  useHeadSafe({
+  useServerHeadSafe({
     ...useLocaleHead({ addSeoAttributes: true }).value,
     bodyAttrs: {
       class:
         'bg-background-bright dark:bg-background-dark font-sans text-text-dark dark:text-text-bright',
     },
-  }) // TODO: use `useServerHeadSafe` (https://github.com/harlan-zw/nuxt-seo-kit/issues/87)
+  })
 
   useServerSeoMeta({
     ...(appConfig.vio.themeColor
