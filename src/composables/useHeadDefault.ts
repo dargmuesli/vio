8d04993f646c02370c08aecd41ca637daa32b1c1
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-09-11 01:06:32 +0200

    fix(head): workaround description

--- src/composables/useHeadDefault.ts
+++ src/composables/useHeadDefault.ts
@@ -11,6 +11,7 @@ export const useHeadDefault = ({
   const attrs = useAttrs()
 
   const defaults: Parameters<typeof useServerSeoMeta>[0] = {
+    description: attrs['site-description'] as string, // TODO: remove (https://github.com/harlan-zw/nuxt-site-config/pull/7)
     msapplicationConfig: `/assets/static/favicon/browserconfig.xml?v=${CACHE_VERSION}`,
     title,
     twitterDescription: attrs['site-description'] as string,
