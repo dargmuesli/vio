58e56c9bbe5e1163e00fc00f47f00d33219a85f6
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-09-14 07:43:40 +0200

    feat(icons): add "arrow left" icon

---
+++ src/components/vio/icon/VioIconArrowLeft.vue
@@ -0,0 +1,9 @@
+<template>
+  <VioIconContainer>
+    <ArrowLeftIcon />
+  </VioIconContainer>
+</template>
+
+<script setup lang="ts">
+import { ArrowLeftIcon } from '@heroicons/vue/24/outline'
+</script>
