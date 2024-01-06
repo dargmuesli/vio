bb33ec7dfb065c2a0e8e059bfab92314ebdfe923
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-01-06 01:35:52 +0100

    perf(polyfills): remove for promise

--- src/composables/usePolyfills.ts
+++ src/composables/usePolyfills.ts
@@ -1,6 +1,8 @@
 import { POLYFILLS } from '../utils/constants'
 
 export const usePolyfills = () => {
+  if (!POLYFILLS.length) return
+
   const polyfillsUrl = `https://polyfill.io/v3/polyfill.min.js?features=${POLYFILLS.join(
     '%2C',
   )}&flags=gated`
