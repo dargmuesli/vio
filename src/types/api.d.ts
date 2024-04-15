634beb87ec6254a680769a9a78ec4053169d8951
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-04-16 01:10:36 +0200

    fix(types): correct backend error

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -6,4 +6,7 @@ export type ApiData = ComputedRef<{
   isFetching: boolean
 }>
 
-export type BackendError = CombinedError | { errcode: string; message: string }
+// if an `originalError` property seems to be required, consider https://github.com/maevsi/maevsi/pull/1182/files#diff-3011f5fc2745796b181fe633b2590cfeb2e976aec95b8e94630bba0cc6163c1cL8
+export type BackendError = {
+  graphQLErrors: (GraphQLError & { errcode?: string })[]
+} & CombinedError
