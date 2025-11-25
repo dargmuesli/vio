a1c037cb87042eb26d91825ea72909a0b415750b
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-11-25 03:44:08 +0100

    refactor(utils): update to `import.meta`

--- src/shared/utils/networking.ts
+++ src/shared/utils/networking.ts
@@ -103,9 +103,7 @@ export const getServiceHref = ({
 
   if (stagingHost) {
     return `https://${nameSubdomainString}${stagingHost}`
-    // TODO: remove disable below (https://github.com/nuxt/nuxt/issues/25323)
-    // eslint-disable-next-line nuxt/prefer-import-meta
-  } else if (isSsr && process.server) {
+  } else if (isSsr && import.meta.server) {
     return `http://${name}${portString}`
   } else {
     return `https://${nameSubdomainString}${getRootHost(host)}`
