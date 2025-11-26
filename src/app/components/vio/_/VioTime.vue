337638f79c5c390e78b160e4bd7bde1505b544a9
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-11-26 04:20:39 +0100

    refactor(components)!: merge time default

--- src/app/components/vio/_/VioTime.vue
+++ src/app/components/vio/_/VioTime.vue
@@ -1,22 +1,31 @@
 <template>
   <NuxtTime
-    v-bind="forwardedProps"
-    :locale="props.locale || defaultLocale"
-    :time-zone="props.timeZone || defaultTimeZone"
+    v-bind="{ datetime: props.datetime, ...delegatedProps }"
+    :locale="props.options.locale || defaultLocale"
+    :time-zone="props.options.timeZone || defaultTimeZone"
   />
 </template>
 
 <script setup lang="ts">
 import { reactiveOmit } from '@vueuse/core'
-import { useForwardProps } from 'reka-ui'
 
 import type { NuxtTimeProps } from 'nuxt/app'
 
 const { locale: defaultLocale } = useI18n()
 const defaultTimeZone = useTimeZone()
 
-const props = defineProps<NuxtTimeProps>()
+const props = withDefaults(
+  defineProps<{
+    datetime: NuxtTimeProps['datetime']
+    options?: Omit<NuxtTimeProps, 'datetime'>
+  }>(),
+  {
+    options: () => ({
+      dateStyle: 'medium',
+      timeStyle: 'short',
+    }),
+  },
+)
 
-const delegatedProps = reactiveOmit(props, 'locale', 'timeZone')
-const forwardedProps = useForwardProps(delegatedProps)
+const delegatedProps = reactiveOmit(props.options, 'locale', 'timeZone')
 </script>
