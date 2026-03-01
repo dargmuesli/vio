3008746b4e58ddeedaac1d6b6caea365d5a1dfda
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-03-01 22:57:06 +0100

    feat(router)!: remove `append` utility

--- src/shared/utils/utils.ts
+++ src/shared/utils/utils.ts
@@ -1,7 +1,2 @@
-import type { RouteLocationRaw } from 'vue-router'
-
-export const append = (path: string, pathToAppend?: RouteLocationRaw) =>
-  path + (path.endsWith('/') ? '' : '/') + (pathToAppend ?? '')
-
 export const arrayRemoveNulls = <T>(array?: T[]) =>
   array?.flatMap((x: T) => (x ? [x] : [])) || []
